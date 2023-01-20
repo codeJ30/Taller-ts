e4770737187ced3abedf8a7e84775a61d657e626
codeJ30 <juancode09@gmail.com>
2023-01-20 13:11:51 -0500

    Solucion al ejercicio 2 (Potencia)

---
+++ src/Ejercicio_02/potencia.ts
@@ -0,0 +1,10 @@
+//@ts-check
+
+
+let resultado ;
+
+let calculaPotencia = (base : number , exponente : number) =>  {
+    return console.log (`El resultado de la potencia al numero ${base} es ${base ** exponente}`)
+}
+
+calculaPotencia(5,2)
