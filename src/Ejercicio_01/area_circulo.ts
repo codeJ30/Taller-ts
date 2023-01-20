54d02d92c5f1375dad9ceded5113b6c950e56582
codeJ30 <juancode09@gmail.com>
2023-01-20 14:43:52 -0500

    Agragando tipos a las variables

--- src/Ejercicio_01/area_circulo.ts
+++ src/Ejercicio_01/area_circulo.ts
@@ -2,10 +2,10 @@
 
 //@ts-check
 
-const PI = 3.14;
-let area;
+const PI : number = 3.14;
+let area : number;
 
-let calcularAreaCirculo = (radio : number) => {
+let calcularAreaCirculo  = (radio : number) => {
 
     return console.log(`El aréa del circulo es ${area = PI * (radio ** 2)}`)
 }
