aebf06471ed295f4cbb06e9b3dfabc5a3efb67c2
codeJ30 <juancode09@gmail.com>
2023-01-20 11:05:58 -0500

    Solución al ejecicio 1

---
+++ src/Ejercicio_01/area_circulo.ts
@@ -0,0 +1,14 @@
+
+
+//@ts-check
+
+const PI = 3.14;
+let area;
+
+let calcularAreaCirculo = (radio : number) => {
+
+    return console.log(`El aréa del circulo es ${area = PI * (radio ** 2)}`)
+}
+
+
+calcularAreaCirculo(4)
